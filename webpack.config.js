ae482783b6eb19e7ba3a5494a03d7f0ee2bb98dc
Hugo Grochau <hugo@grochau.com>
2017-03-24 21:31:54 -0300

    Add react and babel hello world

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'dist');
+var BUILD_DIR = path.resolve(__dirname, 'public');
 var APP_DIR = path.resolve(__dirname, 'src/app');
 
 var config = {
@@ -13,13 +13,12 @@ var config = {
   module: {
     loaders: [
       {
-        test: /\.jsx?/,
+        test: /\.js?/,
         include: APP_DIR,
-        loader: 'babel',
+        loader: 'babel-loader',
       }
     ]
   },
 };
 
 module.exports = config;
-
