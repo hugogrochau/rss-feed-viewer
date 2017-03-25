900bd045cda9a8ffa4c601c469a4bd78f2fe6621
Hugo Grochau <hugo@grochau.com>
2017-03-25 08:45:49 -0300

    Add material-ui and starting components

--- src/app/components/App.js
+++ src/app/components/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import RSSBar from './RSSBar';
+
+injectTapEventPlugin();
 
 const App = () =>
-  <h1> Hello World </h1>;
+  <MuiThemeProvider>
+    <div id="container">
+      <AppBar
+        title="RSS Feed Viewer"
+        iconElementLeft={<span />}
+      />
+      <RSSBar />
+    </div>
+  </MuiThemeProvider>;
 
 export default App;
---
+++ src/app/components/RSSBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TextField from 'material-ui/TextField';
+
+const RSSBar = () =>
+  <TextField
+    hintText="http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+    fullWidth
+  />;
+
+export default RSSBar;
