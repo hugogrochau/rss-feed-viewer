ebe565aa3307209d3e295b91c42f0467e91aeac8
Hugo Grochau <hugo@grochau.com>
2017-03-25 11:36:39 -0300

    Properly use favicon when the RSS feed's image isn't provided

--- src/app/components/RSSFeedViewer/RSSFeedViewerResults/FeedHeaderCard.js
+++ src/app/components/RSSFeedViewer/RSSFeedViewerResults/FeedHeaderCard.js
@@ -7,28 +7,27 @@ import React from 'react';
  */
 class FeedHeaderCard extends React.PureComponent {
   render() {
-    const { url, title, link, author, description } = this.props;
-    const image = this.props.image || `${url}/favicon.ico`;
+    const { title, link, author, description } = this.props;
+    const linkUrl = new URL(link);
+    const image = this.props.image || `${linkUrl.hostname}/favicon.ico`;
 
     return (
       <div>
-      <ul>
-        <li>{url}</li>
-        <li>{title}</li>
-        <li>{link}</li>
-        <li>{author}</li>
-        <li>{description}</li>
-        <li>{image}</li>
-      </ul>
+        <ul>
+          <li>{title}</li>
+          <li>{link}</li>
+          <li>{author}</li>
+          <li>{description}</li>
+          <li>{image}</li>
+        </ul>
       </div>
     );
   }
 }
 
 FeedHeaderCard.propTypes = {
-  url: React.PropTypes.string.isRequired,
   title: React.PropTypes.string.isRequired,
-  link: React.PropTypes.string,
+  link: React.PropTypes.string.isRequired,
   author: React.PropTypes.string,
   description: React.PropTypes.string,
   image: React.PropTypes.string,
